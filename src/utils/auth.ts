796970f9d396a794083cac89581e8a889425cf4a
Zach Plata <zach.plata@webflow.com>
2025-07-09 11:59:35 -0500

    use different env access

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -6,10 +6,6 @@ import * as schema from "../db/schema";
 export const auth = async (envMap: Cloudflare.Env) => {
   const dbInstance = await getDbAsync(envMap.DB);
 
-  console.log("(process env) prod url", process.env.PRODUCTION_ORIGIN_URL);
-  console.log("(import env) prod url", import.meta.env.PRODUCTION_ORIGIN_URL);
-  console.log("(cf env) prod url", envMap.PRODUCTION_ORIGIN_URL);
-
   return betterAuth({
     database: drizzleAdapter(dbInstance, {
       provider: "sqlite",
@@ -18,16 +14,13 @@ export const auth = async (envMap: Cloudflare.Env) => {
     emailAndPassword: {
       enabled: true,
     },
-    secret:
-      process.env.BETTER_AUTH_SECRET || import.meta.env.BETTER_AUTH_SECRET,
-    baseUrl: process.env.BETTER_AUTH_URL || import.meta.env.BETTER_AUTH_URL,
+    secret: envMap.BETTER_AUTH_SECRET,
+    baseUrl: envMap.BETTER_AUTH_URL,
     trustedOrigins: [
       "http://localhost:3000",
       "http://localhost:8787",
-      process.env.BETTER_AUTH_URL || import.meta.env.BETTER_AUTH_URL || "",
-      process.env.PRODUCTION_ORIGIN_URL ||
-        import.meta.env.PRODUCTION_ORIGIN_URL ||
-        "",
+      envMap.BETTER_AUTH_URL || "",
+      envMap.PRODUCTION_ORIGIN_URL || "",
     ],
   });
 };
